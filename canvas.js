947f0d1e6f0599a4c15d0836b0a9cc0619e8eafa
Nicolas Grupe <nico.grupe@gmail.com>
2016-06-28 11:34:25 -0300

    v 5.0

--- canvas.js
+++ canvas.js
@@ -3,13 +3,63 @@
  */
 
 
-function dibujo() {
+function dibujoInicio() {
     var myCanvas = document.getElementById("canvas");
     var ctx = myCanvas.getContext("2d");
     var centerX = myCanvas.width / 2;
     var centerY = myCanvas.height * 2/3;
     var radius = 20;
-    var angIngresado= angulo; //Se supone que este es el valor que ingreso el usuario y esta en grados
+    var angIngresado= 30; //Se supone que este es el valor que ingreso el usuario y esta en grados
+
+    ctx.beginPath();
+    //Dibuja circulo
+    ctx.arc(centerX,centerY,radius,0,2*Math.PI); //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+    //Dibuja Linea
+    ctx.moveTo(centerX,0);
+    ctx.lineTo(centerX,centerY-radius);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    //Si van a cambiar de color, comienzan un Path nuevo
+    ctx.beginPath();
+    ctx.strokeStyle="#194D80";
+    ctx.moveTo(centerX,0);
+    var lineaVer = (centerY-radius) * Math.cos(angIngresado * Math.PI /180 );
+    var lineaHor = (centerY-radius) * Math.sin(angIngresado * Math.PI /180 );
+    ctx.lineTo(centerX-lineaHor,lineaVer); //Linea a la izquierda
+
+    var radioY= radius * Math.cos(angIngresado * Math.PI /180 );
+    var radioX= radius * Math.sin(angIngresado * Math.PI /180 );
+    ctx.arc(centerX-lineaHor-radioX,lineaVer+radioY,radius,-radius,2*Math.PI);
+    ctx.fillStyle = '#194D80';
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    //Si van a cambiar de color, comienzan un Path nuevo
+    ctx.beginPath();
+    ctx.strokeStyle="#7C858B";
+    ctx.moveTo(centerX,0);
+    var lineaVer = (centerY-radius) * Math.cos(angIngresado * Math.PI /180 );
+    var lineaHor = (centerY-radius) * Math.sin(angIngresado * Math.PI /180 );
+    ctx.lineTo(centerX+lineaHor,lineaVer);//Linea a la derecha
+
+    ctx.arc(centerX+lineaHor+radioX,lineaVer+radioY,radius,radius,(Math.PI/180)*360,true);
+    ctx.fillStyle = '#7C858B';
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function dibujo() {
+    var myCanvas = document.getElementById("canvas");
+    var ctx = myCanvas.getContext("2d");
+    var centerX = myCanvas.width / 2;
+    var centerY = cLongitud * 100;
+    var radius = cMasa*4;
+    var angIngresado= cAngulo; //Se supone que este es el valor que ingreso el usuario y esta en grados
 
     ctx.beginPath();
     //Dibuja circulo
--- jsPenduloCónico.js
+++ jsPenduloCónico.js
@@ -2,8 +2,69 @@
  * Created by Mark & Nik on 10/05/16.
  */
 var angulo;
+var cLongitud;
+var cAngulo;
+var cMasa;
+var cRadio;
+
+function Validar()
+{
+
+    if(document.getElementById("tens").value < 0){
+        var t = document.getElementById("tens");
+        t.setAttribute("class", "red");
+        alert("NO SE PUEDEN INGRESAR VALORES NEGATIVOS ");
+    }else{
+        var t = document.getElementById("tens");
+        t.removeAttribute("class");
+    }
+
+
+    if(document.getElementById("masa").value < 0){
+        var m = document.getElementById("masa");
+        m.setAttribute("class", "red");
+        alert("NO SE PUEDEN INGRESAR VALORES NEGATIVOS ");
+    }else{
+        var m = document.getElementById("masa");
+        m.removeAttribute("class");
+    }
+
+
+    if(document.getElementById("angulo").value < 0){
+        var a = document.getElementById("angulo");
+        a.setAttribute("class", "red");
+        alert("NO SE PUEDEN INGRESAR VALORES NEGATIVOS ");
+    }else{
+        var a = document.getElementById("angulo");
+        a.removeAttribute("class");
+    }
+
+
+    if(document.getElementById("radio").value < 0){
+        var r = document.getElementById("radio");
+        r.setAttribute("class", "red");
+        alert("NO SE PUEDEN INGRESAR VALORES NEGATIVOS ");
+    }else{
+        var r = document.getElementById("radio");
+        r.removeAttribute("class");
+    }
+
+
+    if(document.getElementById("longitud").value < 0){
+        var l = document.getElementById("longitud");
+        l.setAttribute("class", "red");
+        
+    }else{
+        var l = document.getElementById("longitud");
+        l.removeAttribute("class");
+    }
+}
+
 function Calcular()
 {
+
+    Validar();
+    
     var resultado=0;
     var unidad;
     var opcion = document.getElementById("varC").value;
@@ -30,7 +91,7 @@ function Calcular()
         case "4"://calculo del Angulo
             resultado = Math.asin(document.getElementById("radio").value / document.getElementById("longitud").value);
 
-            resultado = resultado * 180 / Math.PI; //No convierte correctamente el angulo en Grados
+            //resultado = resultado * 180 / Math.PI; //Convierte correctamente el angulo en Grados
             unidad = "°";
             angulo = resultado;
             break;
@@ -50,6 +111,127 @@ function Calcular()
 
     var resultadoFinal = resultado.toFixed(2);
     document.getElementById("res").value = resultadoFinal + unidad;
+
+    //unidades
+
+    var uMasa = document.getElementById("select-masa").value;
+    if(uMasa == 1){
+        cMasa = document.getElementById("masa").value;
+    }else{
+        cMasa = document.getElementById("masa").value/1000;
+    }
+
+
+    var uAngulo = document.getElementById("select-angulo").value;
+    if(uAngulo == 1){
+        cAngulo = document.getElementById("angulo").value;
+    }else{
+        cAngulo = document.getElementById("angulo").value * 180 / Math.PI;
+    }
+    
+    var uRadio = document.getElementById("select-radio").value;
+    switch(uRadio)
+    {
+        case "1":
+            cRadio = document.getElementById("radio").value;
+            break;
+        case "2":
+            cRadio = document.getElementById("radio").value / 100;
+            break;
+        case "3":
+            cRadio = document.getElementById("radio").value * 1000;
+            break;
+    }
+
+    var uLongitud = document.getElementById("select-longitud").value;
+    switch(uLongitud)
+    {
+        case "1":
+            cLongitud = document.getElementById("longitud").value;
+            break;
+        case "2":
+            cLongitud = document.getElementById("longitud").value / 100;
+            break;
+        case "3":
+            cLongitud = document.getElementById("longitud").value * 1000;
+            break;
+    }
+
     borrarCanvas();
     dibujo();
+}
+
+function desactivar() {
+    var opcion = document.getElementById("varC").value;
+    var t = document.getElementById("tens");
+    var l= document.getElementById("longitud");
+    var m= document.getElementById("masa");
+    var a= document.getElementById("angulo");
+    var r= document.getElementById("radio");
+
+    switch (opcion){
+
+        case "0":
+
+            t.removeAttribute("disabled");
+            l.removeAttribute("disabled");
+            m.removeAttribute("disabled");
+            a.removeAttribute("disabled");
+            r.removeAttribute("disabled");
+            break;
+
+        case "1":
+
+            t.setAttribute("disabled","disabled");
+            l.removeAttribute("disabled");
+            m.removeAttribute("disabled");
+            a.removeAttribute("disabled");
+            r.removeAttribute("disabled");
+            break;
+
+        case "2":
+
+            l.setAttribute("disabled","disabled");
+            t.removeAttribute("disabled");
+            m.removeAttribute("disabled");
+            a.removeAttribute("disabled");
+            r.removeAttribute("disabled");
+            break;
+
+        case "3":
+
+            m.setAttribute("disabled","disabled");
+            t.removeAttribute("disabled");
+            l.removeAttribute("disabled");
+            a.removeAttribute("disabled");
+            r.removeAttribute("disabled");
+            break;
+
+        case "4":
+
+            a.setAttribute("disabled","disabled");
+            t.removeAttribute("disabled");
+            l.removeAttribute("disabled");
+            m.removeAttribute("disabled");
+            r.removeAttribute("disabled");
+            break;
+
+        case "5":
+
+            t.removeAttribute("disabled");
+            l.removeAttribute("disabled");
+            m.removeAttribute("disabled");
+            a.removeAttribute("disabled");
+            r.removeAttribute("disabled");
+            break;
+
+        case "6":
+
+            r.setAttribute("disabled","disabled");
+            t.removeAttribute("disabled");
+            l.removeAttribute("disabled");
+            m.removeAttribute("disabled");
+            a.removeAttribute("disabled");
+            break;
+    }
 }
