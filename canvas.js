f372d5cd3fc0797ce7881e48b0b106205ca4b57b
Nicolas Grupe <nico.grupe@gmail.com>
2016-06-29 10:53:01 -0300

    v 5.4 (Final)

--- canvas.js
+++ canvas.js
@@ -1,15 +1,15 @@
 /**
- * Created by Nicolas Grupe on 11/06/2016.
+ * Created by Nik & Mark on 11/06/2016.
  */
 
-
+//Dibujo predeterminado para la pagina
 function dibujoInicio() {
     var myCanvas = document.getElementById("canvas");
     var ctx = myCanvas.getContext("2d");
     var centerX = myCanvas.width / 2;
     var centerY = myCanvas.height * 2/3;
     var radius = 20;
-    var angIngresado= 30; //Se supone que este es el valor que ingreso el usuario y esta en grados
+    var angIngresado= 30; //
 
     ctx.beginPath();
     //Dibuja circulo
@@ -22,7 +22,7 @@ function dibujoInicio() {
     ctx.stroke();
     ctx.closePath();
 
-    //Si van a cambiar de color, comienzan un Path nuevo
+    //Comienzan un Path nuevo
     ctx.beginPath();
     ctx.strokeStyle="#194D80";
     ctx.moveTo(centerX,0);
@@ -38,7 +38,7 @@ function dibujoInicio() {
     ctx.stroke();
     ctx.closePath();
 
-    //Si van a cambiar de color, comienzan un Path nuevo
+    //Comienzan un Path nuevo
     ctx.beginPath();
     ctx.strokeStyle="#7C858B";
     var lineaVer = (centerY-radius) * Math.cos(angIngresado * Math.PI /180 );
@@ -53,13 +53,15 @@ function dibujoInicio() {
     ctx.closePath();
 }
 
+
+//Dibujo a partir de los datos ingresados por el usuario
 function dibujo() {
     var myCanvas = document.getElementById("canvas");
     var ctx = myCanvas.getContext("2d");
     var centerX = myCanvas.width / 2;
     var centerY = cLongitud * 70;
     var radius = cMasa*4;
-    var angIngresado=0; //Se supone que este es el valor que ingreso el usuario y esta en grados
+    var angIngresado=0; //angulo en grados para que arranque en cero la animacion
 
 function animar (){
 
@@ -75,7 +77,7 @@ function animar (){
     ctx.stroke();
     ctx.closePath();
 
-    //Si van a cambiar de color, comienzan un Path nuevo
+    //Comienzan un Path nuevo
     //Pendulo izquierda
     ctx.beginPath();
     ctx.strokeStyle="#194D80";
@@ -92,7 +94,7 @@ function animar (){
     ctx.stroke();
     ctx.closePath();
 
-    //Si van a cambiar de color, comienzan un Path nuevo
+    //Comienzan un Path nuevo
     //Pendulo Derecha
     ctx.beginPath();
     ctx.strokeStyle="#7C858B";
@@ -118,7 +120,7 @@ function animar (){
 }
 
 
-
+//Borrar  dibujo en el lienzo
 function borrarCanvas() {
     var myCanvas = document.getElementById("canvas");
     var ctx = myCanvas.getContext("2d");
